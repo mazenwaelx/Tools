c9954614cb86a7fe6538eaf9563007803607cf57
mohamedessamx <136021521+mohamedessamx@users.noreply.github.com>
2025-05-07 23:11:42 +0300

    Add files via upload

---
+++ tools/src/main/java/com/tools/dtos/PostDTO.java
@@ -0,0 +1,31 @@
+package com.tools.dtos;
+
+public class PostDTO {
+    private Long userId;
+    private String content;
+    private String imageUrl;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
