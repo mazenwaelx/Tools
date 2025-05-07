15612713129012c02c423c1bedab1632a2d9627b
mohamedessamx <136021521+mohamedessamx@users.noreply.github.com>
2025-05-07 23:25:09 +0300

    Add files via upload

---
+++ tools/src/main/java/com/tools/dtos/CommentDTO.java
@@ -0,0 +1,31 @@
+package com.tools.dtos;
+
+public class CommentDTO {
+    private Long postId;
+    private Long userId;
+    private String content;
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
